d426ca9332d6f430514d2b1fae3606db08adbc56
Flenky85 <TU_EMAIL@loquesea>
2025-10-04 13:10:40 +0200

    Añadido fullattackaftermove

---
+++ CombatOverhaul/Patches/Attack/Patch_AllowFullAttackAfterMove.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using Kingmaker.Controllers.Combat;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CombatOverhaul.Patches.Attack
+{
+    [HarmonyPatch(typeof(UnitCombatState), nameof(UnitCombatState.IsFullAttackRestrictedBecauseOfMoveAction), MethodType.Getter)]
+    public static class Patch_AllowFullAttackAfterMove
+    {
+        static bool Prefix(ref bool __result)
+        {
+            __result = false;
+            return false; // Evita que se ejecute el getter original
+        }
+    }
+}
