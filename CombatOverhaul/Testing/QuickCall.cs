75a396f80ec781ead901077f9c3b05403f3b5a11
Flenky85 <TU_EMAIL@loquesea>
2025-10-04 07:51:25 +0200

    Añadido QuickCall para testear

---
+++ CombatOverhaul/Testing/QuickCall.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+namespace CombatOverhaul.Testing
+{
+    public static class QuickCall
+    {
+        /// <summary>
+        /// Llamar asi en unity explorer: 
+        /// CombatOverhaul.Testing.QuickCall.Run();
+        /// </summary>
+        public static void Run()
+        {
+            try
+            {
+                Debug.Log("[CombatOverhaul][QuickCall] Inicio");
+                // ⬇️ Cambia esta línea por la función real que quieras invocar:
+                QuickCallTest.Test();
+                Debug.Log("[CombatOverhaul][QuickCall] Fin OK");
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError("[CombatOverhaul][QuickCall] ERROR: " + ex);
+            }
+        }
+        public static class QuickCallTest
+        {
+            public static void Test()
+            {
+                Debug.Log("prueba de quickcall");
+            }
+        }
+    }
+}
