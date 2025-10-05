48efd5d8204eb1f20460683d82adba2294990471
Flenky85 <TU_EMAIL@loquesea>
2025-10-05 16:48:12 +0200

    Rename SavingThrow

--- CombatOverhaul/Patches/Roll/SavingThrow.cs
+++ CombatOverhaul/Patches/Roll/SavingThrow.cs
@@ -2,25 +2,22 @@
 using Kingmaker.RuleSystem.Rules;
 using CombatOverhaul.Calculators;
 
-namespace CombatOverhaul.Patches.Saves
+namespace CombatOverhaul.Patches.Roll
 {
-    /// Usa TN derivado de p=(A/(A+D))*α+β para las salvaciones.
     [HarmonyPatch(typeof(RuleSavingThrow), nameof(RuleSavingThrow.IsSuccessRoll))]
-    internal static class Patch_SavingThrow_IsSuccessRoll
+    internal static class SavingThrow
     {
         static bool Prefix(RuleSavingThrow __instance, int d20, int successBonus, ref bool __result)
         {
-            // Naturales como vanilla
             if (d20 == 20) { __result = true; return false; }
             if (d20 == 1) { __result = false; return false; }
 
-            // A = StatValue + successBonus | D = DC
             int A = __instance.StatValue + successBonus;
             int D = __instance.DifficultyClass;
 
             var res = OpposedRollCore.ResolveD20(A, D, d20);
             __result = res.Success;
-            return false; // saltar lógica original
+            return false; 
         }
     }
 }
