dc9451e87ff71a8e098abc753070868db397ae3c
Flenky85 <TU_EMAIL@loquesea>
2025-10-05 16:56:37 +0200

    StrDmagebonus refactorizado

---
+++ CombatOverhaul/Patches/Weapon/RemoveStrDamageBonus.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using Kingmaker.RuleSystem.Rules;
+using Kingmaker.EntitySystem.Stats;
+
+namespace CombatOverhaul.Patches.Weapon
+{
+    [HarmonyPatch(typeof(RuleCalculateWeaponStats), nameof(RuleCalculateWeaponStats.OnTrigger))]
+    internal static class RemoveStrDamageBonus
+    {
+        private static void Prefix(RuleCalculateWeaponStats __instance)
+        {
+            if (__instance == null) return;
+            if (__instance.Weapon == null) return;
+
+            var stat = __instance.DamageBonusStat; 
+            if (stat.HasValue && stat.Value != StatType.Strength) return;
+
+            __instance.OverrideDamageBonusStat(StatType.Strength);
+            __instance.OverrideDamageBonus = 0f;
+        }
+    }
+}
